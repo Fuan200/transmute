20df7885a9333ddac95bddb7d9a39d949d6db9f4
Chase Roohms <chaseroohms@gmail.com>
2026-03-03 14:07:39 -0600

    feat(converters): add PySubs2Converter for subtitle format conversions and include sample subtitle files

--- backend/converters/__init__.py
+++ backend/converters/__init__.py
@@ -4,6 +4,7 @@ from .pandas_convert import PandasConverter
 from .drawio_convert import DrawioConverter
 from .pypandoc_convert import PyPandocConverter
 from .pymupdf_convert import PyMuPDFConverter
+from .pysubs2_convert import PySubs2Converter
 from .converter_interface import ConverterInterface
 
-__all__ = ["FFmpegConverter", "PillowConverter", "PandasConverter", "DrawioConverter", "PyPandocConverter", "PyMuPDFConverter", "ConverterInterface"]
+__all__ = ["FFmpegConverter", "PillowConverter", "PandasConverter", "DrawioConverter", "PyPandocConverter", "PyMuPDFConverter", "PySubs2Converter", "ConverterInterface"]
